ac2a2e87de187f37a9dda395fc71a08d53c50b75
Robert Martinez <robert.martinez@achievers.com>
2024-08-25 11:53:58 -0400

    Initial commit, zsh and kitty configs

---
+++ zsh/.config/zsh/exports.zsh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# HISTFILE="$XDG_DATA_HOME"/zsh/history
+HISTSIZE=1000000
+SAVEHIST=1000000
+export EDITOR="code"
+export TERMINAL="kitty"
+export PATH="$HOME/.local/bin":$PATH
+export PATH="$HOME/.docker/bin":$PATH
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_ENV_HINTS=1
+#export PATH="$PATH:./node_modules/.bin"
+eval "$(starship init zsh)"
+eval "$(zoxide init zsh --cmd cd)"
+# Set up fzf key bindings and fuzzy completion
+source <(fzf --zsh)
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Forgit - git fzf
+[ -f $HOMEBREW_PREFIX/share/forgit/forgit.plugin.zsh ] && source $HOMEBREW_PREFIX/share/forgit/forgit.plugin.zsh
+
+case "$(uname -s)" in
+
+Darwin)
+	# echo 'Mac OS X'
+  export DYLD_LIBRARY_PATH=/opt/homebrew/lib/
+  export DYLD_FALLBACK_LIBRARY_PATH=/opt/homebrew/lib
+	;;
+
+Linux)
+	;;
+
+CYGWIN* | MINGW32* | MSYS* | MINGW*)
+	# echo 'MS Windows'
+	;;
+*)
+	# echo 'Other OS'
+	;;
+esac
---
+++ zsh/.zshrc
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Taken from https://github.com/ChristianChiarulli/machfiles/blob/master/zsh/.zshrc
+
+[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"
+
+
+# history
+HISTFILE=~/.zsh_history
+
+# source
+plug "$HOME/.config/zsh/aliases.zsh"
+plug "$HOME/.config/zsh/exports.zsh"
+plug "$HOME/.config/zsh/functions.zsh"
+
+# plugins
+plug "zsh-users/zsh-autosuggestions"
+plug "hlissner/zsh-autopair"
+plug "zap-zsh/supercharge"
+plug "zap-zsh/fzf"
+plug "zap-zsh/exa"
+plug "zsh-users/zsh-syntax-highlighting"
+plug "zsh-users/zsh-history-substring-search"
+
+# keybinds
+bindkey '^ ' autosuggest-accept
+
+export PATH="$HOME/.local/bin":$PATH
+
+if command -v bat &> /dev/null; then
+  alias cat="bat -pp --theme \"Visual Studio Dark+\""
+  alias catt="bat --theme \"Visual Studio Dark+\""
+fi
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
