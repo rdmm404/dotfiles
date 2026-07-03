cf8ea063317f171a054b2dc6ea91dba3ce25cc6a
Robert Martinez <rdmarenco@uninorte.edu.co>
2026-07-02 21:14:41 -0400

    some more updates

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -21,11 +21,6 @@ eval "$(zoxide init zsh --cmd cd)"
 . "$HOME/.cargo/env"
 export PATH="$HOME/.cargo/bin":$PATH
 
-# pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 
@@ -37,26 +32,21 @@ export AWS_PROFILE=admin
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
 
-# pnpm
-export PNPM_HOME="/Users/$(whoami)/Library/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
-# pnpm end
-
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/$(whoami)/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/$(whoami)/Downloads/google-cloud-sdk/path.zsh.inc'; fi
 
 # The next line enables shell command completion for gcloud.
 if [ -f '/Users/$(whoami)/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/rdmm123/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
 
-
 export XDG_CONFIG_HOME="$HOME/.config/"
+
+# Pi
+export PATH="/home/rdmm123/.local/share/fnm/node-versions/v24.18.0/installation/bin:$PATH"
 
 case "$(uname -s)" in
 
 Darwin)
+  export PNPM_HOME="/Users/$(whoami)/Library/pnpm"
 	# echo 'Mac OS X'
   export DYLD_LIBRARY_PATH=/opt/homebrew/lib/
   export DYLD_FALLBACK_LIBRARY_PATH=/opt/homebrew/lib
@@ -65,12 +55,14 @@ Darwin)
 	;;
 
 Linux)
+  export PNPM_HOME="$HOME/.pnpm"
   export PATH="$PATH:/snap/bin"
+  export HOST_IP=$(ip route | awk '/default/ { print $3 }')
   if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
     # forwarding GUIs to windows for WSL
     # https://medium.com/@matthewkleinsmith/headful-playwright-with-wsl-4bf697a44ecf
     # https://aalonso.dev/blog/2021/how-to-use-gui-apps-in-wsl2-forwarding-x-server-cdj
-    export DISPLAY=$(ip route | awk '/default/ { print $3 }'):1.0
+    export DISPLAY="$HOST_IP:1.0"
     export LIBGL_ALWAYS_INDIRECT=true
     export LIBGL_ALWAYS_SOFTWARE=true
   fi
@@ -88,4 +80,9 @@ CYGWIN* | MINGW32* | MSYS* | MINGW*)
 *)
 	# echo 'Other OS'
 	;;
+esac
+
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
